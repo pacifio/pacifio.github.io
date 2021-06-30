895e6f5493b588fbf0e31f73c94d8604eac4695e
Adib Mohsin <adibmohsin.root@gmail.com>
2021-06-30 18:12:36 +0600

    Updated site for bigger screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    purge: ["./index.html"],
-    darkMode: false,
-    theme: {
-        extend: {
-            fontFamily: {
-                'cabin': ['Cabin',],
-            },
-        },
+  purge: ["./index.html"],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        cabin: ["Cabin"],
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
